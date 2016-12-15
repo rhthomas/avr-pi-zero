0d07df2b850ee5334b44e91935c84cbe5c72e665
Rhys Thomas <rhys97@me.com>
2016-12-15 15:35:39 +0000

    Overflow is now 16 bits to allow frequency range of 0-1M. Initialised the serial comms (forgot to do this in main).

--- fcount.c
+++ fcount.c
@@ -12,16 +12,14 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "debug.h"
+#include <avr/interrupt.h>
 
 // probably more space efficient than #include <bool.h>
 typedef enum {false, true} bool;
 
-// flag whether rising edge of input
-bool first = false;
-// number times the system overflows
-uint8_t overflow=0;
-// calculated frequency from ext. interrupt
-float new_freq;
+bool first = false; // flag whether rising edge of input
+uint16_t overflow=0; // number times the system overflows
+float new_freq=0; // calculated frequency from ext. interrupt
 
 void extInterrupt(void)
 {
@@ -37,6 +35,9 @@ void extInterrupt(void)
 ISR(INT0_vect)
 {
     first = ~first;
+    // led to indicate interrupt has triggered
+    PORTB ^= _BV(PB4);
+    sendStrBySerial("interrupt\r\n");
 
     if(first) {
         // enable overflow interrupt
@@ -69,18 +70,22 @@ ISR(TIMER1_OVF)
 
 int main(void)
 {
+    DDRB |= _BV(PB4);
+    PORTB &= ~_BV(PB4);
+
     // initialise timer
     timer1_init();
     // initialise external interrupts
     extInterrupt();
+    initSerial(PB3);
     sei();
 
     char str[80];
 
     for(;;){
-        sendStrBySerial("running\r\n");
         sprintf(str,"freq: %f\r\n",new_freq);
         sendStrBySerial(str);
+        _delay_ms(250);
     }
 
     return 0;
