8f2c08d7bebb4b39753054434e03012bf0442fee
Rhys Thomas <rhys97@me.com>
2016-12-16 00:57:09 +0000

    Frequency counter works for input frequencies >~17Hz, anything less comes out at around 17Hz. This could be due to the time to move floats around the cpu and printing over UART etc. One needs to look into better methods of timing. eg,  * Stopping timer1 completly when calculating freq and starting again when count == 1.  * Circular buffers.  * Record a number of samples then find an average.  * etc. After that we can then work on adding the comparator on an ADC input to measure sinusiodal waves. The comparator will be like a rising edge on an external interrupt (or PCIE) so the main functionality will be almost identical.

--- fcount.c
+++ fcount.c
@@ -3,21 +3,22 @@
 // created : 2016-12-14
 /* frequency counter. run with -u vfprintf -lprintf_flt
  * todo:
- * [ ] get it working with 1Hz square wave
+ * [x] get it working with 1Hz square wave
+ *     ^ sort of, anything >20Hz works.
+ *     ^ anything less comes out as 20Hz, this must be times to calculate freq etc?
  * [ ] use comparator on ADC (see T3 lab)
  */
 
 #define F_CPU 1000000L // 1MHz internal clock
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdbool.h>
 #include "debug.h"
-
-typedef enum {false, true} bool;
 
 volatile bool print=false; // flag to tell main to print
 volatile uint16_t overflow=0; // number times the system overflows
-volatile float new_freq=0.0; // calculated frequency from ext. interrupt
-volatile uint8_t count=0;
+volatile float freq;
+volatile uint8_t count;
 
 void timer1_init(void)
 {
@@ -25,7 +26,7 @@ void timer1_init(void)
     TIMSK |= _BV(TOIE1); // enable overflow interrupt
 }
 
-void extInterrupt(void)
+void external_init(void)
 {
     MCUCR |= _BV(ISC01) | _BV(ISC00); // trigger interrupt on rising edge
     GIMSK |= _BV(INT0); // set external interrupt
@@ -35,16 +36,15 @@ ISR(INT0_vect)
 {
     count++;
 
-    // if first rising edge, reset counters
+    // if first rising edge, start counters
     if(count == 1) {
-        TCNT1 = 0;
         overflow = 0;
+        TCNT1 = 0;
         PORTB |= _BV(PB4);
     }
     // else, calculate freq and reset count
     else {
-        float total_ticks = TCNT1+(overflow*255);
-        new_freq = 1000000.0/total_ticks;
+        freq = 1000000.0/(TCNT1+(overflow*255));
         count = 0; // reset count
         print = true;
         PORTB &= ~_BV(PB4);
@@ -52,24 +52,25 @@ ISR(INT0_vect)
 }
 
 // count number of overflows
-// this interrupts comms so ISR_NOBLOCK needed
+// this effects comms so ISR_NOBLOCK needed
 ISR(TIMER1_OVF_vect,ISR_NOBLOCK){overflow++;}
 
 int main(void)
 {
+    // debug leds
     DDRB |= _BV(PB4);
     PORTB &= ~_BV(PB4);
 
     initSerial(PB3);
     timer1_init();
-    extInterrupt();
+    external_init();
     sei();
 
-    char str[80];
+    char str[10];
 
     for(;;){
         if(print) {
-            sprintf(str,"freq: %.2f\r\n",new_freq);
+            sprintf(str,"%.2fHz\r\n",freq);
             sendStrBySerial(str);
             print = false; // reset print flag
         }
