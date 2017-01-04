9b72f25d6279dbaf3882554cc409a1cd41062794
Rhys Thomas <rhys97@me.com>
2017-01-04 21:30:49 +0000

    shiftInit now takes number of 595's connected in chain.

--- sipo.h
+++ sipo.h
@@ -7,16 +7,18 @@
  */
 
 #include <avr/io.h>
+#include <math.h>
 
 volatile struct {
-    uint8_t data, latch, clock;
+    uint8_t data, latch, clock, num;
 } regs = {0,0,0};
 
-void shiftInit(uint8_t dataPin, uint8_t latchPin, uint8_t clockPin)
+void shiftInit(uint8_t dataPin, uint8_t latchPin, uint8_t clockPin, uint8_t num)
 {
     regs.data = _BV(dataPin);
     regs.latch = _BV(latchPin);
     regs.clock = _BV(clockPin);
+    regs.num = num; // number of 595's connected in chain
 
     // set control pins as outputs and initialise low
     DDRB |= (regs.data | regs.latch | regs.clock);
@@ -25,9 +27,9 @@ void shiftInit(uint8_t dataPin, uint8_t latchPin, uint8_t clockPin)
 
 void shiftOut(uint8_t val)
 {
-    for(uint8_t i=0; i<8; i++) {
+    for(uint8_t i=0; i<(8*regs.num); i++) {
         // tests MSB of val
-        if(val & 0b10000000) {
+        if(val & pow(2,regs.num-1)) { // 0b10000000 for one 595
             PORTB |= regs.data;
         }
         else {
